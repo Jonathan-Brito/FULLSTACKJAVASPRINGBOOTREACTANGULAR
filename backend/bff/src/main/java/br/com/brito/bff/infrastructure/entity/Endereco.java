87175dbb5d98d5f1ec17cfa62adb2c6987372c95
Jonathan Brito Costa <jonathan.costa@itsgroup.com.br>
2025-06-20 12:35:01 -0300

    Criado estrutura e implementado classes de entidade

--- backend/bff/src/main/java/br/com/brito/bff/infrastructure/entity/Endereco.java
+++ backend/bff/src/main/java/br/com/brito/bff/infrastructure/entity/Endereco.java
@@ -1,4 +1,38 @@
 package br.com.brito.bff.infrastructure.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "endereco")
 public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "rua", length = 200)
+    private String rua;
+
+    @Column(name = "numero", length = 10)
+    private Long numero;
+
+    @Column(name = "complemento", length = 10)
+    private String complemento;
+
+    @Column(name = "cidade", length = 150)
+    private String cidade;
+
+    @Column(name = "estado", length = 100)
+    private String estado;
+
+    @Column(name = "cep", length = 9)
+    private String cep;
 }
